88c0b1aebeba42ccb8bf7db48dbeb49ee7281617
Nasser <nasseratic@gmail.com>
2025-02-21 14:42:59 +0200

    fix Mushaf page cut off

--- src/components/Mushaf/MushafPage.tsx
+++ src/components/Mushaf/MushafPage.tsx
@@ -1,12 +1,14 @@
 import { QuranPageImageMap } from "assets/quran_images";
 import { APPBAR_HEIGHT } from "components/AppBar";
-import { SCREEN_WIDTH } from "constants/GeneralConstants";
+import { SCREEN_WIDTH, IS_IOS } from "constants/GeneralConstants";
 import { Image } from "expo-image";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { View } from "tamagui";
 
 export const ImageMushafPage = ({ pageNumber }: { pageNumber: number }) => {
+  const insets = useSafeAreaInsets();
   return (
-    <View px={8} f={1} w={SCREEN_WIDTH} pt={APPBAR_HEIGHT}>
+    <View px={8} f={1} w={SCREEN_WIDTH} pt={APPBAR_HEIGHT} pb={(IS_IOS ? 40 : 90) + insets.bottom}>
       <Image
         style={{
           width: "100%",
