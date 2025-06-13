e96c64d413c9317ef60d5e9076485569f39cc9cc
Nasser <nasseratic@gmail.com>
2025-06-13 13:30:43 +0200

    fix mushaf page top part hidden

--- src/components/Mushaf/MushafPage.tsx
+++ src/components/Mushaf/MushafPage.tsx
@@ -2,21 +2,30 @@ import { QuranPageImageMap } from "assets/quran_images";
 import { SCREEN_WIDTH, IS_IOS } from "constants/GeneralConstants";
 import { Image } from "expo-image";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { View } from "tamagui";
+import { Stack, View } from "tamagui";
+import { MUSHUF_PAGE_HEADER_HEIGHT } from "./MushafPagesHeader";
 
 export const ImageMushafPage = ({ pageNumber }: { pageNumber: number }) => {
   const insets = useSafeAreaInsets();
   return (
-    <View px={8} f={1} w={SCREEN_WIDTH} pb={(IS_IOS ? 40 : 90) + insets.bottom}>
-      <Image
-        style={{
-          width: "100%",
-          flex: 1,
-        }}
-        contentFit="contain"
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        source={QuranPageImageMap[pageNumber as keyof typeof QuranPageImageMap]}
-      />
-    </View>
+    <Stack>
+      <View
+        px={8}
+        f={1}
+        w={SCREEN_WIDTH}
+        pt={MUSHUF_PAGE_HEADER_HEIGHT + insets.top}
+        pb={(IS_IOS ? 40 : 90) + insets.bottom}
+      >
+        <Image
+          style={{
+            width: "100%",
+            flex: 1,
+          }}
+          contentFit="contain"
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+          source={QuranPageImageMap[pageNumber as keyof typeof QuranPageImageMap]}
+        />
+      </View>
+    </Stack>
   );
 };
